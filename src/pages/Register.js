74b874196105109b3cba9cee1c3d604622dd774a
unknown <edswinningattitude@gmail.com>
2024-04-02 21:07:43 -0600

    stupid change to restart server

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useTitle } from "../hooks/useTitle";
-import { toast } from "react-toastify";
 import { register } from "../services/authService";
+import { toast } from "react-toastify";
 
 export const Register = () => {
   useTitle("Register");
